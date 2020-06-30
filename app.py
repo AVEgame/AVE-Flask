b0b9a8218c97db20a3d91a6f1cb3fc6ad10d197e
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2020-06-30 18:51:04 +0100

    Add docs to site

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template
 from datetime import datetime
+import markdown
+import re
 
 app = Flask(__name__)
 
+with open("templates/ave.html", "r") as f:
+    AVE_SPANS = f.read()
+
 @app.context_processor
-def inject_now():
+def inject():
     return {
         'now': datetime.utcnow(),
+        'version': 1.9,
         }
 
 @app.route('/')
 def index():
-    return render_template("index.html", version=1.9)
+    return render_template("index.html")
+
+@app.route('/docs/<filename>')
+def docs(filename):
+    with open(f"AVE-docs/docs/{filename}", "r") as f:
+        text = f.read()
+    html = markdown.markdown(text, extensions=['tables', 'fenced_code']).replace(" AVE ", f" {AVE_SPANS} ")
+    html = re.sub(r"\%\%([^\%]*)\%\%", r'<span style="color:#4d9906"><<i>\1</i>></span>', html)
+    return render_template("docs.html", content=html)
+
