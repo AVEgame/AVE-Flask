707d1a772fd0aee427a3e9bc6b87929bd3c34f74
Giancarlo Grasso <ggrasso@blueyonder.co.uk>
2020-07-10 16:56:35 +0100

    Fix for reverse proxies

--- app/app.py
+++ app/app.py
@@ -31,7 +31,7 @@ class ReverseProxied(object):
         self.app = app
 
     def __call__(self, environ, start_response):
-        scheme = environ.get('X-Forwarded-Proto')
+        scheme = environ.get('HTTP_X_FORWARDED_PROTO')
         if scheme:
             environ['wsgi.url_scheme'] = scheme
         return self.app(environ, start_response)
